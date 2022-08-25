9cb0c000558ab6af543a7feabe82823296721349
ibktommy <atomquartz@gmail.com>
2022-08-25 13:39:17 +0100

    Setting up the Error Page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-const Error = () => {
-  return <h2>error page</h2>;
-};
+
+// Creating the Wrapper-styled Component
 const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
@@ -18,4 +17,17 @@ const Wrapper = styled.section`
     margin-bottom: 1.5rem;
   }
 `;
+
+const Error = () => {
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>sorry, the page you tried cannot be found!</h3>
+        <Link to='/' className='btn'>Back to home</Link>
+      </div>
+    </Wrapper>
+  );
+};
+
 export default Error;
