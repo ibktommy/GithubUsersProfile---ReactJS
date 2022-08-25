09f0d2a9dea1e4f1764b2e4584ae7dcf5d7a4655
ibktommy <atomquartz@gmail.com>
2022-08-25 15:18:19 +0100

    Setting up custom mockData for the webapp

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
+import mockUser from './mockData.js/mockUser.json';
+import mockRepos from './mockData.js/mockRepos.json';
+import mockFollowers from './mockData.js/mockFollowers.json';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
