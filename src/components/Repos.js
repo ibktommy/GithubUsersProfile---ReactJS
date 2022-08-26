09c2dae0165a5bdc259ea587f78d7856a74c343f
ibktommy <atomquartz@gmail.com>
2022-08-26 19:48:38 +0100

    Creating a sample chart using FusionCharts dependency

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import { FusionChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+
 const Repos = () => {
-  return <h2>repos component</h2>;
+  return (
+    <FusionChart />
+  );
 };
 
+// ADD STYLING
 const Wrapper = styled.div`
   display: grid;
   justify-items: center;
