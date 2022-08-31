f1f3433cae3e39e7840db55ac28743698f389d07
ibktommy <atomquartz@gmail.com>
2022-08-31 16:48:38 +0100

    Wrapping the webapp with Auth0

--- src/index.js
+++ src/index.js
@@ -6,9 +6,17 @@ import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+// dev - loa3ld - d.us.auth0.com
+// zQBGlUcHwwC9DgKSixA1aamDJYqOHcRw
+
 ReactDOM.render(
-  <GithubProvider>
-    <App />
-  </GithubProvider>,
+  <Auth0Provider 
+    domain= "dev - loa3ld - d.us.auth0.com">
+    clientId="zQBGlUcHwwC9DgKSixA1aamDJYqOHcRw"
+    redirectUri={window.location.origin}
+    <GithubProvider>
+      <App />
+    </GithubProvider>
+  </Auth0Provider>,
   document.getElementById('root')
 );
