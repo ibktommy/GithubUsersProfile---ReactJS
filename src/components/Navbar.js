29cc95c6372b09693d2b231f3a31375950037e1b
ibktommy <atomquartz@gmail.com>
2022-08-31 18:04:32 +0100

    Checking if the user login was succesful and then using the object values gotten from the Auth0 to display the name and picture of the user that logged-in

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,12 +15,14 @@ const Navbar = () => {
   // Setting Variable that checks if isAuthenticated and user is defined
   const isUser = isAuthenticated && user
 
-  
-
-  console.log({isAuthenticated, user, isLoading})
+  // console.log({isAuthenticated, user, isLoading})
 
   return (
     <Wrapper>
+      {isUser && user.picture && <img src={user.picture} alt={user.name}/>}
+      
+      {isUser && user.name && <h4>Welcome, <strong>{user.name.toUpperCase()}</strong></h4>}
+
       <button onClick={loginWithRedirect}>Login</button>
       <button onClick={() => { logout({ returnTo: window.location.origin }) }}>Logout</button>
     </Wrapper>
